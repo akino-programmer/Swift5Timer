03c7d205829f53302733159466a8597050ef93ba
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-20 21:56:28 +0900

    start and stop function.

--- Swift5Timer/ViewController.swift
+++ Swift5Timer/ViewController.swift
@@ -10,10 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+     //stopButtonが押せなくなる
+        stopButton.isEnabled = true
+        
+        
     }
+    
+    @IBAction func start(_ sender: Any) {
+        //imageViewのimageに順番に反映していく
+        
+        
+        
+        //startButtonは押せなくなる
+        startButton.isEnabled = true
+        
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        //imageViewのimageに反映されている画像の流れをストップする
+        
+        
+        //startButtonが押せるようになる
+        startButton.isEnabled = false
+        
+    }
+    
 
 
 }
