f9d53ec178163af2e2b335bd6974f977312fb795
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-21 13:43:42 +0900

    timer function.

--- Swift5Timer/ViewController.swift
+++ Swift5Timer/ViewController.swift
@@ -16,25 +16,61 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var stopButton: UIButton!
     
+    var timer = Timer()
+    
+    var count  = Int()
+    
+    var imageArray = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        count = 0
      //stopButtonが押せなくなる
-        stopButton.isEnabled = true
+        stopButton.isEnabled = false
+        
+        for i in 0..<5{
+                 
+                 print(i)
+                 
+                 let image = UIImage(named: "\(i)")
+                 imageArray.append(image!)
+                 
+                 
+             }
+        
+            imageView.image = UIImage(named: "0")
+        
+    }
+    
+    func startTimer(){
+        //タイマーを回す0.2秒毎にあるメソッドを呼ぶ
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(timerUpdate), userInfo: nil, repeats: true)
+    }
+    //0.2秒毎に呼ばれる
+    @objc func timerUpdate(){
+        
+        count = count + 1
+        
+        if count  > 4 {
+            
+            count =  0
+        }
+        
+        imageView.image = imageArray[count]
         
         
     }
     
     @IBAction func start(_ sender: Any) {
         //imageViewのimageに順番に反映していく
-        
-        
+    
+        startTimer()
         
         //startButtonは押せなくなる
-        startButton.isEnabled = true
-        
+        startButton.isEnabled = false
+        stopButton.isEnabled = true
     }
     
     @IBAction func stop(_ sender: Any) {
@@ -42,7 +78,13 @@ class ViewController: UIViewController {
         
         
         //startButtonが押せるようになる
-        startButton.isEnabled = false
+        startButton.isEnabled = true
+        stopButton.isEnabled = false
+        
+        //タイマーを止める
+        timer.invalidate()
+        
+       
         
     }
     
